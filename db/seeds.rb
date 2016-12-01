1317cdac21ecf8f1d4d60fd210dcc8994ada837f
kiraamaa <kira.a.maclean@gmail.com>
2016-12-01 11:37:09 -0500

    Add seed data
    Cheeseplates populated with 6 plates
    Cheese populated with 12 cheeses

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# cheeseplates = Cheeseplate.create([{ name: 'Nutty' }, { name: 'Piquant' }, { name: 'Floral' }, { name: 'Buttery' }, { name: 'Earthy' }, { name: 'Tangy' }])
+cheeses = Cheese.create([
+  { name: 'Beaufort',
+  description: 'This giant 80 pound cheese is fruity and nutty. Hailing from the French Alps, it is a great melter and is often added to fondue.' },
+  { name: 'Saint Marcellin',
+  description: 'A soft, rindless cow\'s milk cheese from Dauphine, it is disk shaped wrapped in chestnut leaves and dipped in wine or eau-de-vie. It has an intensely rustic, nutty, fruity flavor.' },
+  { name: 'Gouda',
+  description: 'Gouda is a semisoft to hard natural cheese, depending on age. It is pale yellow and slightly sweet and nutty.' },
+  { name: 'd"Allevo Asiago',
+  description: 'From the high plateau of the same name, d\'Allevo Asiago is made from whole unpasteurized milk and is firm, sharp, and nutty.' },
+  { name: 'Montasio',
+  description: 'From Friuli, this mountain cheese has a firm texture and a smooth, nutty flavor with hints of grass and honey.' },
+  { name: 'Mahon',
+  description: 'An aged cheese produced from cow\'s milk on Minorca, the outermost of the three Spanish Balearic Islands. Ripened for six months to two years, it is buttery sharp, slightly salty with a sweet and nutty aroma.' }])
--- scripts/update-cheeseplate.sh
+++ scripts/update-cheeseplate.sh
@@ -2,6 +2,6 @@ curl --include --request PATCH http://localhost:4741/cheeseplates/6 \
   --header "Content-Type: application/json" \
   --data '{
     "cheeseplate": {
-      "name": "Soapy",
+      "name": "Tangy"
     }
   }'
