8cf7f9803da686b995fe2e90887cbff84e98b7f6
kiraamaa <kira.a.maclean@gmail.com>
2016-12-01 11:14:21 -0500

    Created cheese table
    Now have cheeseplates and cheeses as empty objects

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   resources :cheeseplates, only: [:index, :show, :create, :update, :destroy]
+  resources :cheeses, only: [:index, :show, :create, :update, :destroy]
 end
